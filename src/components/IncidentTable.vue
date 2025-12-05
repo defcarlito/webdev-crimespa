f907770f017092de70646f247e44a2ec47452f9d
harrisonamorim <harrisoncamorim@gmail.com>
2025-12-05 14:47:42 -0600

    table component

---
+++ src/components/IncidentTable.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { neighborhoodToString } from "../scripts/neighborhood.js";
+
+const props = defineProps({
+  incidents: {
+    type: Array,
+    required: true,
+  },
+});
+
+function formatDateString(dateStr) {
+  return dateStr;
+}
+
+function formatTimeString(timeStr) {
+  return timeStr;
+}
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Case #</th>
+        <th>Type</th>
+        <th>Neighborhood</th>
+        <th>Date</th>
+        <th>Time</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="inc in incidents">
+        <td>{{ inc.case_number }}</td>
+        <td>{{ inc.incident }}</td>
+        <td>{{ neighborhoodToString(inc.neighborhood_number) }}</td>
+        <td>{{ formatDateString(inc.date) }}</td>
+        <td>{{ formatTimeString(inc.time) }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
