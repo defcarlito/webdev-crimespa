68e5497eb8cbc7bcc51fcb739c2f0ce4b2da9fed
harrisonamorim <harrisoncamorim@gmail.com>
2025-12-10 12:51:27 -0600

    about page skeleton

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,60 @@
-<script setup></script>
+<script setup>
+import TeamMembers from "../json/team-members.json";
+</script>
 
 <template>
-  <div>
-    <h1>this is a test</h1>
+  <div class="main-container">
+    <div>
+      <h1 class="text-center">Team Members</h1>
+      <div class="grid-x align-center grid-margin-x">
+        <div
+          v-for="member in TeamMembers"
+          class="cell medium-shrink small-12 text-center"
+        >
+          <img :src="member.imgUrl" alt="team member photo" class="portrait" />
+          <h3>{{ member.name }}</h3>
+          <p>{{ member.description }}</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h1 class="text-center">Video Overview</h1>
+      <!-- placeholder video for now -->
+      <iframe
+        class="grid-x align-center"
+        src="https://www.youtube.com/watch?v=PZ_ebxkNZmo"
+      ></iframe>
+    </div>
+    <div>
+      <h1 class="text-center">Interesting Findings</h1>
+      <ul class="interesting-findings">
+        <li>Thing 1</li>
+        <li>Thing 2</li>
+        <li>Thing 3</li>
+        <li>Thing 4</li>
+        <li>Thing 5</li>
+        <li>Thing 6</li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<style>
+.main-container {
+  margin: 10vh;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+.interesting-findings {
+  list-style-position: inside;
+  padding-left: 0;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.portrait {
+  width: 256px;
+}
+</style>
